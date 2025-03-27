daf5ac584fc64bc9451e6923b0c5e7316c593618
JaidityaB <jaiditya.bansal33@gmail.com>
2025-03-27 09:39:08 +0530

    started cart project

---
+++ week-8/cart/src/components/nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function nav() {
+  return (
+    <div>
+      <img src='../assets/logo_linkedcart.png' alt='logo' />
+      <FontAwesomeIcon icon='fa-regular fa-user' />
+    </div>
+  );
+}
