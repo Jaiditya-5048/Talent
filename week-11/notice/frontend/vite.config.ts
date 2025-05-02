919388c9dc9cf4dc7934dcf9b56d460248f0278a
JaidityaB <jaiditya.bansal33@gmail.com>
2025-05-02 18:40:40 +0530

    draggable frontend done port congif to 5173

--- week-11/notice/frontend/vite.config.ts
+++ week-11/notice/frontend/vite.config.ts
@@ -7,6 +7,6 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
-    port: 5000, // Replace 3000 with your desired port number
+    port: 5173,
   },
 });
